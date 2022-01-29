3c936e9c010cdb7e1ed7deec35934a3e966e50db
newname58q <85175546+newname58q@users.noreply.github.com>
2022-01-29 22:31:33 +0300

    2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# abone
+# Telegram_Live_Subscriber_Count
+Telegram Channel Live Subscriber Counting Bot
+
+
+### Deploy to Heroku
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/newname58q/abone)
---
+++ bot.py
@@ -0,0 +1,33 @@
+# © mrlokaman , @lntechnical
+import os
+from pyrogram import Client, filters
+import time 
+ADMIN =int(os.environ.get("ADMIN", 923943045))
+TOKEN = os.environ.get("TOKEN", "")
+
+API_ID = int(os.environ.get("API_ID", 12345))
+
+API_HASH = os.environ.get("API_HASH", "")
+app = Client(
+        "live",
+        bot_token=TOKEN,api_hash=API_HASH,
+            api_id=API_ID
+    )
+        
+CH_USER_NAME = os.environ.get("CH_USER_NAME", "")
+x = []
+    
+@app.on_message(filters.user(ADMIN) & filters.command("livecount",prefixes='!'))
+async def livecount(client,message):
+  count1 = await app.get_chat_members_count(CH_USER_NAME)
+  x.append(count1)
+  ms = await message.reply_text(f"Canlı Telegram Abone Sayma\n {CH_USER_NAME} - {count1}")
+  while True :
+   time.sleep(3)
+   count = await app.get_chat_members_count(CH_USER_NAME)  
+   if x[0] != count:
+       del x[0]
+       x.append(count)
+       await ms.edit(f"Canlı Telegram Abone Sayma\n {CH_USER_NAME} - {x[0]}")
+ 
+app.run()
